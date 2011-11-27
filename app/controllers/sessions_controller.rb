7d2750fe4aa3f91d2187cf235448367f2199971d
Geoff Massanek <gmassanek@gmail.com>
2011-11-27 14:17:29 -0600

    sets reminder and public defaults on link form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    session[:user_id] = nil
+    reset_session
     redirect_to root_path, :notice => "Logged out successfully"
   end
 
