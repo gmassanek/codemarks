0102d11aded13168fb79bf705d9e590d8ddfab9c
Geoff Massanek <gmassanek@gmail.com>
2011-11-27 13:18:46 -0600

    Getting rid of

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -13,20 +13,20 @@ class Topic < ActiveRecord::Base
   has_many :sponsored_sites, :inverse_of => :topic, :dependent => :destroy
   accepts_nested_attributes_for :sponsored_sites, :reject_if => lambda {|s| s[:url].blank? || s[:site].blank? }
 
-  scope :by_resource_count, select("`topics`.*")
-                      .joins("LEFT JOIN `link_topics` ON `link_topics`.topic_id = `topics`.id")
-                      .group("`link_topics`.topic_id")
-                      .order("count(`link_topics`.id) DESC")
+  scope :by_resource_count, select("topics.*")
+                      .joins("LEFT JOIN link_topics ON link_topics.topic_id = topics.id")
+                      .group("link_topics.topic_id")
+                      .order("count(link_topics.id) DESC")
 
 
   scope :by_recent_activity, select('distinct topics.*')
-                            .joins('LEFT JOIN `link_topics` ON `link_topics`.topic_id = topics.id')
-                            .order('`link_topics`.created_at DESC')
+                            .joins('LEFT JOIN link_topics ON link_topics.topic_id = topics.id')
+                            .order('link_topics.created_at DESC')
 
   scope :mine, lambda { |user_id| 
-                joins('INNER JOIN `link_topics` mylt on `mylt`.topic_id = `topics`.id')
-                .joins('INNER JOIN `link_saves` on `link_saves`.link_id = `mylt`.link_id')
-                .where(['`link_saves`.user_id = ?', user_id]) }
+                joins('INNER JOIN link_topics mylt on mylt.topic_id = topics.id')
+                .joins('INNER JOIN link_saves on link_saves.link_id = mylt.link_id')
+                .where(['link_saves.user_id = ?', user_id]) }
 
 end
 
