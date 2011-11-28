45b2a74ab4c9bb10d2e3be759e85979d5bc6ade8
Geoff Massanek <gmassanek@gmail.com>
2011-11-28 15:18:48 -0600

    Added resource count function to topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -27,5 +27,16 @@ class Topic < ActiveRecord::Base
   scope :by_recent_activity, select('DISTINCT topics.*')
                             .joins('LEFT JOIN link_topics ON link_topics.topic_id = topics.id')
                             .order('link_topics.created_at DESC')
+
+  def resource_count current_user_id, filter_by_mine
+    if current_user_id.nil?
+      self.links.public.count
+    elsif filter_by_mine
+      self.links.mine(current_user_id).count
+    else
+      self.links.public_or_mine(current_user_id).count
+    end
+  end
+
 end
 
