acc1866b25c2895a73685481435ba78f37991806
Geoff Massanek <gmassanek@gmail.com>
2011-12-20 10:11:42 -0600

    Footer and other stuff

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Cms::Application.routes.draw do
   get '/welcome', to: "users#welcome", as: :welcome
   get '/profile', to: "users#profile", as: :profile
   get '/profile/edit', to: "users#edit", as: :edit_profile
+  get '/about', to: "pages#about", as: :about
   get '/codemarks/build_link', to: "codemarks#build_link", as: :build_link
   get '/widget', to: "pages#widget", as: :widget
   post '/codemarks', to: "codemarks#create", as: :codemarks
