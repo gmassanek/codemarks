fea800a4bcdd36f401051f2c506afd47eca192ee
Geoff Massanek <gmassanek@gmail.com>
2011-12-19 23:09:51 -0600

    inline topics back

--- app/controllers/codemarks_controller.rb
+++ app/controllers/codemarks_controller.rb
@@ -30,7 +30,14 @@ class CodemarksController < ApplicationController
         @codemark.link = Link.new params[:codemark][:link]
       end
       @codemark.user = current_user
-      @codemark.topics = Topic.find params[:topic_ids].keys
+      @codemark.topics = Topic.find params[:topic_ids].keys.reject! { |topic| topic.match(/[^0-9]/) }
+
+      new_topics = params[:topic_ids].keys
+      new_topics.reject! { |topic| !topic.match(/[^0-9]/) }
+      new_topics.each do |title|
+        @codemark.topics << Topic.new(title: title, user: current_user)
+      end
+
       if Codemarker.mark! @codemark
         redirect_to dashboard_path, notice: "Codemark created"
       else
