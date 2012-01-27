43ac421b2976fe194920d2def30fbb73fc417fa2
Geoff Massanek <gmassanek@gmail.com>
2012-01-27 01:01:52 -0600

    Change links to non-active-record objects. Currently, it has replace smart_links. Need to figure out persistance and relationships now

--- spec/models/links_spec.rb
+++ spec/models/links_spec.rb
@@ -1,42 +1,81 @@
-require 'spec_helper'
+require 'active_record'
+require_relative '../../app/models/link'
 
 describe Link do
-  describe "URLs" do
-    it "gets all links for a list of codemarks" do
-      codemark1 = Fabricate(:codemark)
-      codemark2 = Fabricate(:codemark)
-      Link.for([codemark1, codemark2]).should == [codemark1.link, codemark2.link]
+  let(:valid_url) { "http://www.example.com" }
+
+  describe "#initialize" do
+    it "sets the link's url" do
+      link = Link.new(valid_url)
+      link.url.should == valid_url
     end
 
-    describe "validations" do
-      let (:link) { Fabricate.build(:link) }
-
-      it "are required" do
-        link.url = nil
-        link.should_not be_valid
+    # TODO Make these offline tests - probably perfect for VCR
+    # TODO analyze the stubbing here - might need wrapper class for URI
+    describe "#gathers_site_response" do
+      it "is non-nil for valid urls" do
+        link = Link.new(valid_url)
+        link.site_response.should_not be_nil
       end
 
-      ["http://www.twitter.com", "https://twitter"].each do |url|
-        it "accepts #{url}" do
-          link.url = url
-          link.should be_valid
-        end
-      end
-
-      ["twitter.com", "twitter", "www.twitter.com"].each do |url|
-        it "rejects #{url}" do
-          link.url = url
-          link.should_not be_valid
+      invalid_urls = ["twitter.com", "twitter", "www.twitter.com"]
+      invalid_urls.each do |url|
+        it "is nil for invalid urls like #{url}" do
+          link = Link.new(url)
+          link.site_response.should be_nil
         end
       end
     end
 
-    describe "topics" do
-      it "are associated through codemarks" do
-        github = Fabricate(:topic, title: "Github")
-        codemark = Fabricate(:codemark, topics: [github])
-        codemark.link.topics.should == [github]
+    describe "#parse_site_response" do
+      let(:link) { Link.new("") }
+
+      it "does nothing if the site data is blank" do
+        link.parse_site_response
+
+        link.title.should be_nil
+        link.site_content.should be_nil
+        link.host.should be_nil
+      end
+
+      it "sets the title" do
+        site_response = stub(title: "Github").as_null_object
+        link.stub!(site_response: site_response)
+
+        link.parse_site_response
+        link.title.should == "Github"
+      end
+
+      it "sets the actual html content" do
+        content = '<tag with="attributes" that=\'have weird stuff\'>and data</tag>'
+        site_response = stub(content: content).as_null_object
+        link.stub!(site_response: site_response)
+
+        link.parse_site_response
+        link.site_content.should == content
+      end
+
+      it "sets the host" do
+        host = 'www.example.com'
+        link = Link.new(valid_url)
+        link.host.should == host
       end
     end
   end
 end
+
+#it "gets all links for a list of codemarks" do
+#  codemark1 = Fabricate(:codemark)
+#  codemark2 = Fabricate(:codemark)
+#  Link.for([codemark1, codemark2]).should == [codemark1.link, codemark2.link]
+#end
+#
+#    describe "topics" do
+#      it "are associated through codemarks" do
+#        github = Fabricate(:topic, title: "Github")
+#        codemark = Fabricate(:codemark, topics: [github])
+#        codemark.link.topics.should == [github]
+#      end
+#    end
+#  end
+#end
