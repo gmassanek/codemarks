c6d92a33c49dfe8154df3927ca73a079ecece10e
Geoff Massanek <gmassanek@gmail.com>
2012-01-31 22:39:00 -0600

    All model specs passing - though not sure if there's great coverage. Going to step up to Controller specs to see if any of it worked

---
+++ app/models/codemarks/codemark.rb
@@ -0,0 +1,14 @@
+module Codemarks
+  class Codemark
+    attr_accessor :link
+
+    def initialize(url=nil)
+      @link = Codemarks::Link.new(url)
+    end
+
+    def commit
+      @link.commit
+    end
+
+  end
+end
