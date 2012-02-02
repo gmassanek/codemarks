267a8309b44fc9e02ea0aa0b84c7e0bab461a3b7
Geoff Massanek <gmassanek@gmail.com>
2012-02-01 22:06:40 -0600

    prepare linkmark is tested and implemented in codemarks controller. Probably could stub a bit better

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,8 @@
 class Link < ActiveRecord::Base
+  has_many :topics, :through => :codemarks
+  has_many :codemarks, :dependent => :destroy
+  has_many :clicks
+
   validates_presence_of :url, :site_content, :host, :title
 end
 #scope :for, lambda { |codemarks| joins(:codemarks).where(['codemarks.id in (?)', codemarks]) }
