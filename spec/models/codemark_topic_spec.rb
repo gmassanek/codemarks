32b886022cf528e7451e56391b6a7180b70e74a3
Geoff Massanek <gmassanek@gmail.com>
2012-02-26 21:13:30 -0600

    Fixed up a couple codemark specs, mostly associations

--- spec/models/codemark_topic_spec.rb
+++ spec/models/codemark_topic_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe CodemarkTopic do
   it "has a codemark and a topic" do
     codemark_topic = Fabricate.build(:codemark_topic)
-    codemark_topic.codemark.should_not be_nil
+    codemark_topic.codemark_record.should_not be_nil
     codemark_topic.topic.should_not be_nil
   end
 end
