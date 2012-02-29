a8d144a0e786b4e09d6d298d7bd4cb68b70903c8
Geoff Massanek <gmassanek@gmail.com>
2012-02-28 22:26:04 -0600

    Model name changes to dashboard specs, fixed a session controller bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
     session[:filter] = "mine"
     session[:sort] = "by_save_date"
 
-    if @user.codemarks.count == 0
+    if FindCodemarks.new(:user => @user).codemarks.count == 0
       @link = Link.new
       redirect_to welcome_path
     else
@@ -24,6 +24,7 @@ class SessionsController < ApplicationController
     end
 
   rescue Exception => ex
+    p "Exception in app/controllers/sessions_controller.rb A"
     logger.info ex.to_s
     puts ex.to_s
     redirect_to root_path, :notice => "Sorry, something went wrong"
