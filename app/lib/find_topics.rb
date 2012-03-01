6402332c52c062c36bf49eee1218fa0d6e0306dd
Geoff Massanek <gmassanek@gmail.com>
2012-02-29 21:43:21 -0600

    FindTopics should only take a resource, not a resource.id

--- app/lib/find_topics.rb
+++ app/lib/find_topics.rb
@@ -1,6 +1,5 @@
 class FindTopics
-  def self.existing_topics_for(taggable_class, resource_id)
-    resource = taggable_class.send(:find_by_id, resource_id)
-    resource.topics
+  def self.existing_topics_for(resource)
+    resource.send(:topics)
   end
 end
