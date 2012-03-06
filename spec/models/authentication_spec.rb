b496218233354ae5de7bc35a52c64d4a606cb304
Geoff Massanek <gmassanek@gmail.com>
2012-03-05 20:32:22 -0600

    Authentications require a nickname (github and twitter always provide one)

--- spec/models/authentication_spec.rb
+++ spec/models/authentication_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Authentication do
   context "requires" do
-    [:provider, :uid, :user].each do |field|
+    [:provider, :uid, :user, :nickname].each do |field|
       it "a #{field}" do
         authentication = Fabricate.build(:authentication, field.to_sym => nil)
         authentication.should_not be_valid
