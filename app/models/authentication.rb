29dcba8ef28165aa0b62feddc80bc118604c8e17
Geoff Massanek <gmassanek@gmail.com>
2012-03-05 20:32:22 -0600

    Authentications require a nickname (github and twitter always provide one)

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,8 +1,8 @@
 class Authentication < ActiveRecord::Base
 
   belongs_to :user
-  validates_presence_of :provider, :uid
   validates_presence_of :user
+  validates_presence_of :provider, :uid, :nickname
 
   def self.find_by_provider_and_uid(provider, uid)
     find(:first, :conditions => ["provider = ? AND uid = ?", provider, uid])
