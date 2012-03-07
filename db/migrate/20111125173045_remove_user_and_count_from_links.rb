9ca0312f0e8416ad0cf98c75248152b722ed91a7
Geoff Massanek <gmassanek@gmail.com>
2012-03-07 08:57:02 -0600

    Cleaning migrations

--- db/migrate/20111125173045_remove_user_and_count_from_links.rb
+++ db/migrate/20111125173045_remove_user_and_count_from_links.rb
@@ -1,11 +1,9 @@
 class RemoveUserAndCountFromLinks < ActiveRecord::Migration
   def up
     remove_column :links, :user_id
-    remove_column :links, :save_count
   end
 
   def down
     add_column :links, :user_id, :integer
-    add_column :links, :save_count, :integer
   end
 end
