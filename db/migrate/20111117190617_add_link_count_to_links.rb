de83011738918b2e52306657383e783dc847f3ce
Geoff Massanek <gmassanek@gmail.com>
2012-03-07 08:57:02 -0600

    Cleaning migrations

--- db/migrate/20111117190617_add_link_count_to_links.rb
+++
@@ -1,5 +0,0 @@
-class AddLinkCountToLinks < ActiveRecord::Migration
-  def change
-    add_column :links, :clicks_count, :integer, :default => 0
-  end
-end
--- db/migrate/20111125163839_add_times_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddTimesToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :created_at, :datetime
-    add_column :users, :updated_at, :datetime
-  end
-end
