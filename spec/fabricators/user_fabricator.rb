0a43f8859b1b7df2ae15a5d98fd1010fd18561bf
Geoff Massanek <gmassanek@gmail.com>
2012-03-07 22:24:30 -0600

    Skipping js integration specs so that travis will work

--- spec/fabricators/user_fabricator.rb
+++ spec/fabricators/user_fabricator.rb
@@ -2,6 +2,14 @@ Fabricator(:user) do
   authentications(:count => 1) { |user| Fabricate(:authentication, :user => user) }
 end
 
+Fabricator(:twitter_user, :class_name => "user") do
+  authentications(:count => 1) { |user| Fabricate(:authentication, :user => user, :provider => "twitter") }
+end
+
+Fabricator(:github_user, :class_name => "user") do
+  authentications(:count => 1) { |user| Fabricate(:authentication, :user => user, :provider => "github") }
+end
+
 Fabricator(:active_user, :from => :user) do
   after_create do |user|
     3.times { Fabricate(:link_save, :user => user) }
