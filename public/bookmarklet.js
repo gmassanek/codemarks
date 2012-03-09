3e1355d11a18c5628c6f207a4d3f04b39e4c58d5
Geoff Massanek (s) <geoff@massanek@gmail.com>
2012-03-09 14:58:18 -0500

    Cleaning up from server

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -8,11 +8,16 @@ function cml1() {
       var z=d.createElement('script');
       z.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
       b.appendChild(z);
+    } else {
+      var z=d.createElement('script');
+      z.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js');
+      b.appendChild(z);
 
       var z=d.createElement('script');
-      z.setAttribute('src','https://www.codemarks.org/autocomplete-rails.js.js');
+      z.setAttribute('src','http://www.codemarks.org/autocomplete-rails.js');
       b.appendChild(z);
     }
+
     waitForJquery(0);
   }
   catch(e){
@@ -23,6 +28,14 @@ function cml1() {
 function waitForJquery(cnt) {
   if(cnt>20) return;
   if(window.jQuery) {
+    var z=document.createElement('script');
+    z.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js');
+    document.body.appendChild(z);
+
+    var z=document.createElement('script');
+    z.setAttribute('src','http://www.codemarks.org/autocomplete-rails.js');
+    document.body.appendChild(z);
+
     $.ajax({
       url: "http://www.codemarks.org/listener/prepare_bookmarklet?id=USER_ID&l="+l,
       context: document.body,
