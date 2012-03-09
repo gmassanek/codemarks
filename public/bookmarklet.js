848caeffaefc19556e0e23836004719005489f25
Geoff Massanek <gmassanek@gmail.com>
2012-03-09 07:55:17 -0600

    [ci skip] two step codemarklet, attempt 1 (need to find a way to test this locally)

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,22 +1,36 @@
-function cml1()
-  {
-    var d=document;
-    var z=d.createElement('scr'+'ipt');
-    b=d.body;
-    l=d.location;
-    try{
-      if(!b) throw(0);
+function cml1() {
+  var d=document;
+  var b=d.body;
+  l=d.location;
+  try{
+    if(!b) throw(0);
+    if(!window.jQuery) {
+      var z=d.createElement('script');
       z.setAttribute('src','https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js');
       b.appendChild(z);
-      var req = $.ajax({
-        url: "http://www.codemarks.org/listener/bookmarklet?id=USER_ID&l="+l,
-        dataType: "script"
-      });
-      console.log(req);
     }
-    catch(e){
-      alert('Please wait until the page has loaded.');
-    }
+    waitForJquery(0);
   }
+  catch(e){
+    console.log(e);
+    alert('Please wait until the page has loaded.');
+  }
+}
+function waitForJquery(cnt) {
+  if(cnt>20) return;
+  if(window.jQuery) {
+    $.ajax({
+      url: "http://www.codemarks.org/listener/prepare_bookmarklet?id=USER_ID&l="+l,
+      context: document.body,
+      success: function(response) {
+        console.log(response);
+        $(this).append(response);
+      }
+    });
+  } else {
+    window.setTimeout(function(){waitForJquery(cnt+1)}, 100);
+  }
+}
 cml1();
-void(0)
+void(0);
+
