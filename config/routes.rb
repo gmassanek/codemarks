6a5e17895ae1db162a62eb0268f4becaf6e37ad6
Geoff Massanek <gmassanek@gmail.com>
2012-03-10 16:38:40 -0600

    cleaning up routes a bit, really nasty still though

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,31 @@
 Codemarks::Application.routes.draw do
 
-  get '/public', to: "codemarks#public", :as => :public_codemarks
   get '/dashboard', to: "users#dashboard", as: :dashboard
   get '/welcome', to: "users#welcome", as: :welcome
   get '/profile', to: "users#profile", as: :profile
   get '/profile/edit', to: "users#edit", as: :edit_profile
   get '/about', to: "pages#about", as: :about
-  resources :codemarks, :only => [:edit]
+
+  resources :codemarks
   get '/codemarks/build_linkmark', to: "codemarks#build_linkmark", as: :build_linkmark
-  get '/widget', to: "pages#widget", as: :widget
-  post '/codemarks', to: "codemarks#create", as: :codemarks
+  get '/public', to: "codemarks#public", :as => :public_codemarks
 
   post 'listener/sendgrid', :to => "listener#sendgrid", as: "sendgrid_listener"
   get 'listener/prepare_bookmarklet', :to => "listener#prepare_bookmarklet", as: "prepare_bookmarklet"
   get 'listener/bookmarklet', :to => "listener#bookmarklet", as: "bookmarklet"
 
   match 'auth/:provider/callback', to: 'sessions#create'
-  get '/about', :to => 'pages#about', :as => :about_path
   get 'pages/autocomplete_topic_title', :as => :topic_title_autocomplete
-  get '/links/topic_checkbox', :to => "links#topic_checkbox"
 
   root :to => 'pages#landing'
 
   resources :topics
-  resources :links
-  resources :users
-  resources :reminders, :only => [:create]
 
+  resources :links
+  get '/links/topic_checkbox', :to => "links#topic_checkbox"
   post 'links/click', :as => :click_link
 
+  resources :users
   get "sessions/new", :as => :sign_in
   post "sessions/create", :as => :create_session
   post "sessions/filter", :as => :filter_session
