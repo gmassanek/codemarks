106d21b260642d54bf6090b8f4917a25ace849ff
Geoff Massanek <gmassanek@gmail.com>
2012-03-11 16:37:37 -0500

    When viewing another user's show page, I see a tab for his username so I know what I'm looking at

--- features/step_definitions/view_codemarks_steps.rb
+++ features/step_definitions/view_codemarks_steps.rb
@@ -27,6 +27,12 @@ Given /^there are (\d+) codemarks for "([^"]*)"$/ do |num_codemarks, topic_title
   end
 end
 
+Given /^([^"]*) is a user with a codemark$/ do |nickname|
+  @user = Fabricate(:user)
+  @user.update_attributes(:nickname => nickname)
+  @codemark = Fabricate(:codemark_record, :user => @user)
+end
+
 When /^I go to the "([^"]*)" topic page$/ do |topic_title|
   visit topic_path(@topic)
 end
@@ -37,6 +43,21 @@ end
 
 When /^I go to the public page$/ do
   visit '/public'
+end
+
+When /^I go to his page$/ do
+  visit '/public'
+  page.click_link @user.nickname
+end
+
+Then /^I should see his codemark$/ do
+  page.should have_content(@codemark.title)
+end
+
+Then /^I should see a tab with his name$/ do
+  within('#filters_and_sorts .tabs') do
+    page.should have_content(@user.nickname)
+  end
 end
 
 Then /^I should see (\d+) codemarks$/ do |num|
