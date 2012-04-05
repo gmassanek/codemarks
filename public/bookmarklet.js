286a212fb2cf411f724ea96c9d4b274f2e5f756b
Geoff Massanek <gmassanek@gmail.com>
2012-04-05 00:34:27 -0500

    Or do it the simple way, without jquery or popup windows and just use basic JS

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,11 +1,12 @@
-function start() {
-  if(!window.Codemarklet) {
-    jqueryScriptTag = document.createElement('script');
-    jqueryScriptTag.setAttribute('src','http://localhost:3000/javascripts/codemarklet.js');
-    document.body.appendChild(jqueryScriptTag);
-  } else {
-    window.Codemarklet.start();
-  }
-}
+window.codemarklet=window.codemarklet||{};
+var D=550;
+var A=450;
+var C=screen.height;
+var B=screen.width;
+var H=Math.round((B/2)-(D/2));
+var G=0;
+var F=document;
+var E;
 
-start();
+if(C>A){G=Math.round((C/2)-(A/2))}
+window.codemarklet.shareWin=window.open('http://staging.codemarks.com/','','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
