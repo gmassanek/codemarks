9a505b052f4974ed9f2bfa78aa3573f747398e1b
Geoff Massanek <gmassanek@gmail.com>
2012-04-09 22:02:53 -0500

    Resizing/breaking apart bookmarklet.js

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,12 +1,12 @@
+var loc = document.location
+var server = 'http://codemarks.com/';
 window.codemarklet=window.codemarklet||{};
-var D=550;
-var A=450;
-var C=screen.height;
-var B=screen.width;
-var H=Math.round((B/2)-(D/2));
+var H=450;
+var W=604;
+var SW=screen.width;
+var SH=screen.height;
+var LFT=Math.round((SW/2)-(W/2));
 var G=0;
-var F=document;
-var E;
-
-if(C>A){G=Math.round((C/2)-(A/2))}
-window.codemarklet.shareWin=window.open('http://staging.codemarks.com/codemarklet/new?url=' + document.location,'','left='+H+',top='+G+',width='+D+',height='+A+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
+var url = server + 'codemarklet/new?url=' + loc;
+if(SH>H){G=Math.round((SH/W)-(H/2))}
+window.codemarklet.shareWin=window.open(url,'','left='+LFT+',top='+G+',width='+W+',height='+H+',personalbar=0,toolbar=0,scrollbars=1,resizable=1');
