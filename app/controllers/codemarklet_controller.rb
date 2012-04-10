c8893c5e251b3d56292a8ef95c2639ec52b37b18
Geoff Massanek <gmassanek@gmail.com>
2012-04-09 22:42:41 -0500

    Adding required login and forematted codemarklet

--- app/controllers/codemarklet_controller.rb
+++ app/controllers/codemarklet_controller.rb
@@ -2,8 +2,13 @@ class CodemarkletController < ApplicationController
   layout 'codemarklet'
 
   def new
+    redirect_to login_codemarklet_index_path unless logged_in?
     options = {}
     options[:url] = params[:url]
     @codemark = Codemark.load(options)
+  end
+
+  def login
+    p "woooo"
   end
 end
