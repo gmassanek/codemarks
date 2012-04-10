f08090374c415ee0dc718aabb802bca41bcbc747
Geoff Massanek <gmassanek@gmail.com>
2012-04-09 23:46:37 -0500

    Bookmarklet might be done??

--- public/bookmarklet.js
+++ public/bookmarklet.js
@@ -1,5 +1,5 @@
-var loc = 'http://coderetreatfloyd2012.eventbrite.com';
-var server = 'http://localhost:3000/';
+var loc = document.location;
+var server = 'http://staging.codemarks.com/';
 window.codemarklet=window.codemarklet||{};
 var H=450;
 var W=604;
