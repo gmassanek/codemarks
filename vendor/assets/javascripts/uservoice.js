56ea012af00bdf7cda92e89f1ce5b7111b2e9d17
Geoff Massanek <gmassanek@gmail.com>
2012-05-01 23:04:15 -0500

    Adding uservoice

---
+++ vendor/assets/javascripts/uservoice.js
@@ -0,0 +1,6 @@
+var uvOptions = {};
+(function() {
+  var uv = document.createElement('script'); uv.type = 'text/javascript'; uv.async = true;
+  uv.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'widget.uservoice.com/xs8Hnw3G0BKoJ7vdVNF3Ig.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(uv, s);
+})();
