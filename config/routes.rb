3cab7ace2935c57a1abe4cb56977b74bb9de1378
Geoff Massanek <geoff@groupon.com>
2012-05-16 22:36:59 -0500

    More route cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -7,32 +7,38 @@ Codemarks::Application.routes.draw do
   get '/pages/test_bookmarklet?:l&:url', :controller => :pages, :action => :test_bookmarklet, :as => :test_bookarklet
 
   resources :codemarklet, :only => [:new, :create] do
-    collection { get :login }
-    collection { get :chrome_extension }
+    collection do
+      get :login
+      get :chrome_extension
+    end
   end
+
   resources :codemarks, :only => [:new, :create, :destroy] do
     collection do
       get 'search'
+      get 'topic_checkbox'
     end
   end
   get '/public', to: "codemarks#public", :as => :public_codemarks
-
-  match 'auth/:provider/callback', to: 'sessions#create'
-  match 'auth/failure', to: 'sessions#failure'
 
   resources :comments, :only => [:create, :destroy]
   resources :topics
   get 'topics/:id/:user_id', :to => 'topics#show', :as => 'topic_user'
 
-  get '/codemarks/topic_checkbox'
   resources :links, :only => [] do
-    member { post :click }
+    member do
+      post :click
+    end
   end
 
-  get "sessions/codemarklet_sign_in", :as => :codemarklet_sign_in
-  post "sessions/create", :as => :create_session
-  post "sessions/filter", :as => :filter_session
-  delete "sessions/destroy", :as => :sign_out
+  match 'auth/:provider/callback', to: 'sessions#create'
+  match 'auth/failure', to: 'sessions#failure'
+  resources :sessions, :only => [:create] do
+    collection do
+      get :codemarklet
+      delete :destroy
+    end
+  end
 
   resources :users, :only => [:show, :update]
   get '/:id', :to => 'users#show', :as => "short_user"
