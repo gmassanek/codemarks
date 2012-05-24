967b195268e4ff9ef0dbe2c11f8c719a6846a00f
Geoff Massanek <gmassanek@gmail.com>
2012-05-23 22:43:52 -0500

    Hide twitter auth keys

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, "3svARFoOWB2BK5m8Aiw5ug", "tb0skqupVv7mLeOFCioiBqwGUNEusM6Suzhc2IYt0" 
+  #provider :provider, consumer_key, consumer_secret
+  provider :twitter, ENV['CODEMARK_TWITTER_KEY'], ENV['CODEMARK_TWITTER_SECRET']
   provider :github, "17d564cb5975e018dd77", "e98387e2eb75b7a52f970d91e7e7f053220dc9e6"
 end
 
