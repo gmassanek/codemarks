cb47135095bef3d57f710c7cb61b2a3db9bfe9ec
Geoff Massanek <gmassanek@gmail.com>
2012-06-26 18:08:40 -0400

    Convert github omniauth keys to evo variables

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :provider, consumer_key, consumer_secret
   provider :twitter, ENV['CODEMARK_TWITTER_KEY'], ENV['CODEMARK_TWITTER_SECRET']
-  provider :github, "17d564cb5975e018dd77", "e98387e2eb75b7a52f970d91e7e7f053220dc9e6"
+  provider :github, ENV['CODEMARK_GITHUB_KEY_2'], ENV['CODEMARK_GITHUB_SECRET_2']
+  provider :github, ENV['CODEMARK_GITHUB_KEY_1'], ENV['CODEMARK_GITHUB_SECRET_1']
 end
 
