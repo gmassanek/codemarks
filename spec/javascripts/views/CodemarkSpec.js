544516e55d9e5767ff3c1dd1a006fde014653286
Geoff Massanek <gmassanek@gmail.com>
2012-07-01 12:33:31 -0400

    Git ignore JS specs - just use `foreman start` to generate new ones

--- spec/javascripts/views/CodemarkSpec.js
+++ spec/javascripts/views/CodemarkSpec.js
@@ -1,18 +1,5 @@
 (function() {
 
-  describe('Codemark', function() {
-    return it('does not have a save link if it\'s already mine', function() {
-      var codemark, codemarkView;
-      codemark = new App.Models.Codemark({
-        mine: true
-      });
-      codemarkView = new App.Views.Codemark({
-        model: codemark
-      });
-      codemarkView.render();
-      expect(codemarkView.$('.copy_codemark').length).toBe(0);
-      return expect(codemarkView.$('.title').length).toBe(1);
-    });
-  });
+  describe('Codemark', function() {});
 
 }).call(this);
