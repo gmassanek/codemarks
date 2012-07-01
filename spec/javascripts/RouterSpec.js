d170155dfb685093e8c20e22a1ca12c4889a903e
Geoff Massanek <gmassanek@gmail.com>
2012-07-01 12:33:31 -0400

    Git ignore JS specs - just use `foreman start` to generate new ones

--- spec/javascripts/RouterSpec.js
+++ spec/javascripts/RouterSpec.js
@@ -3,11 +3,16 @@
   describe('Router', function() {
     var router;
     router = new App.MainRouter;
-    it('routes home to index', function() {
-      return expect(router.routes['codemarks']).toEqual('index');
+    describe('routes pages like', function() {
+      it('public to Codemarks#public', function() {
+        return expect(router.routes['public']).toEqual('public');
+      });
+      return it('passes showCodemarkList to succes? - Should you test what function you pass?', function() {});
     });
-    return it('routes public to index', function() {
-      return expect(router.routes['public']).toEqual('public');
+    return describe('#showCodemarkList', function() {
+      it('has better tests than the next to that know all about implementation', function() {});
+      it('renders the CodemarkList view with the incoming codemarks', function() {});
+      return it('appends that to the dom', function() {});
     });
   });
 
