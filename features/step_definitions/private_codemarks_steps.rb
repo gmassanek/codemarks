cb2a8b9b47b2ed1a1f0cf6982034dc9907a9c354
Geoff Massanek <gmassanek@gmail.com>
2012-10-28 16:29:36 -0500

    Removing old cukes, fixing good ones

--- features/step_definitions/private_codemarks_steps.rb
+++ features/step_definitions/private_codemarks_steps.rb
@@ -1,28 +1,19 @@
-When /^I fill out the form with Twitter$/ do
-  Fabricate(:topic, :title => 'twitter')
-  page.fill_in('url', :with => 'http://www.twitter.com')
-  page.click_button('fetch')
-  wait_until { find('#codemark_title').visible? }
-end
-
-When /^I submit the form$/ do
-  page.click_button('fetch')
-end
-
 Given /^I add the 'private' tag$/ do
-  Fabricate(:topic, :title => 'private')
-  page.fill_in('link_form_topic_autocomplete', :with => 'private')
-  page.find('.ui-menu-item a').click()
+  select('private', :from => 'topics')
 end
 
 Given /^I have a private codemark$/ do
-  private = Fabricate(:topic, :title => 'private')
-  @codemark = Fabricate(:codemark_record, :topics => [private], :user => @current_user)
+  @codemark = Fabricate(:codemark_record, :topics => [@private], :user => @current_user)
 end
 
 Given /^another user has a private codemark$/ do
   @user = Fabricate(:user)
   @codemark = Fabricate(:codemark_record, :user => @user, :private => true)
+end
+
+Given /^a private tag exists$/ do
+  @private = Topic.find_by_title('private')
+  @private = Fabricate(:topic, :title => 'private') unless @private
 end
 
 Then /^that codemark should be private$/ do
