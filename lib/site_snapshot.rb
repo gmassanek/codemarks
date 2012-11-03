5e994fee5d3aa93c08ac002dd0f21e801504b14c
Geoff Massanek <gmassanek@gmail.com>
2012-11-03 17:10:42 -0500

    Fix spec

--- lib/site_snapshot.rb
+++ lib/site_snapshot.rb
@@ -1,9 +1,11 @@
 require 'grabzitclient'
 
 class SiteSnapshot
+  WIDTH = '705'
+  HEIGHT = '642'
   def self.save_picture(url, filepath)
     #grabzItClient = GrabzItClient.new(ENV['GRABZIT_KEY'], ENV['GRABZIT_SECRET'])
     grabzItClient = GrabzItClient.new('Z21hc3NhbmVrQGdtYWlsLmNvbQ==', 'GXQ/VT8Efi0/Pz8hPyk/P2YCOz9HGD84P3wVPw0/M1Q=')
-    grabzItClient.save_picture(url, filepath, '705', '642')
+    grabzItClient.save_picture(url, filepath, WIDTH, HEIGHT)
   end
 end
