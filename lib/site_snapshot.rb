6e0563655943151f33d5a9c147bad061bc7642cb
Geoff Massanek <gmassanek@gmail.com>
2012-11-03 14:08:07 -0500

    New tile look

--- lib/site_snapshot.rb
+++ lib/site_snapshot.rb
@@ -4,6 +4,6 @@ class SiteSnapshot
   def self.save_picture(url, filepath)
     #grabzItClient = GrabzItClient.new(ENV['GRABZIT_KEY'], ENV['GRABZIT_SECRET'])
     grabzItClient = GrabzItClient.new('Z21hc3NhbmVrQGdtYWlsLmNvbQ==', 'GXQ/VT8Efi0/Pz8hPyk/P2YCOz9HGD84P3wVPw0/M1Q=')
-    grabzItClient.save_picture(url, filepath)
+    grabzItClient.save_picture(url, filepath, '705', '642')
   end
 end
