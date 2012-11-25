fa63cde033555434b3126231b9026c113f506632
Geoff Massanek <gmassanek@gmail.com>
2012-11-24 23:01:02 -0600

    Move migration to work

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019005513) do
+ActiveRecord::Schema.define(:version => 20121124040354) do
 
   create_table "authentications", :force => true do |t|
     t.string   "uid"
