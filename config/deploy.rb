e19429e68877801b88ff1d27459a49558356c6a9
Geoff Massanek <gmassanek@gmail.com>
2012-12-10 22:16:17 -0600

    Deploy master now that this is merged

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :use_sudo, false
 
 set :scm, "git"
 set :repository, "git@github.com:gmassanek/#{application}.git"
-set :branch, "new-server"
+set :branch, "master"
 
 default_run_options[:pty] = true
 ssh_options[:forward_agent] = true
