2a73bedf80da49b019c56f608320fc97db683e18
Geoff Massanek <gmassanek@gmail.com>
2012-12-26 20:12:55 -0500

    Add unicorn error log and change unicorn socket

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,9 +1,9 @@
 root = "/home/deployer/apps/codemarks/current"
 working_directory root
 pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
+stderr_path "#{root}/log/unicorn_err.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.codemarks.sock"
 worker_processes 2
 timeout 30
