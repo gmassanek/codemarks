1eb53d44d3990b86370e576a7e0845c182a9ba19
Geoff Massanek <gmassanek@gmail.com>
2012-12-29 17:25:00 -0500

    Use one common set of omniauth stubs for dev, rspec, and cukes

---
+++ spec/support/omniauth.rb
@@ -0,0 +1,21 @@
+OmniAuth.config.test_mode = true
+OmniAuth.config.add_mock(:twitter, {
+  :uid => '987877',
+  :info => {
+    :name => "Geoff Massanek",
+    :image => "https://si0.twimg.com/profile_images/1269676102/twit2_bigger.jpg",
+    :location => "Chicago, IL",
+    :description => "The baddest twitter monster on the planet",
+    :nickname => "gmassanek"
+  }
+})
+
+OmniAuth.config.add_mock(:github, {
+  :uid => '223498',
+  :info => {
+    :name => "Github Monster",
+    :image => "http://www.gravatar.com/avatar/58dbba1be3de0ccf3a495e978bdcb220.png",
+    :location => "Chicago, IL",
+    :nickname => "gmassanek"
+  }
+})
