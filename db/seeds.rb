233a27deb18895c350f5894fdf720dac342cb70a
Geoff Massanek <gmassanek@gmail.com>
2012-12-29 17:21:33 -0500

    Add webmock and vcr to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,11 @@
 require_relative 'seeder'
+require 'webmock'
+include WebMock::API
+require Rails.root.join("spec/support/vcr.rb")
+
+VCR.configure do |config|
+  config.default_cassette_options = { :re_record_interval => 180.days }
+end
 
 Seeder.clear_database
 Seeder.create_topics(['commit', 'codemarks', 'rspec', 'capybara', 'jquery', 'css tricks'])
@@ -6,7 +13,7 @@ gmassanek = Seeder.create_me
 jbeiber = Seeder.create_user('jbeiber')
 carlyraejepsen = Seeder.create_user('carlyraejepsen')
 
-urls = [
+my_urls = [
   'https://github.com/gmassanek/codemarks/pull/63',
   'http://rspec.info/',
   'https://github.com/rspec/rspec-core',
@@ -16,7 +23,10 @@ urls = [
   'http://mylesmegyesi.blogspot.com/2011/11/tree-traversal-with-tail-recursion.html',
   'http://mylesmegyesi.blogspot.com/2011/11/minimax-depth.html',
   'http://paulirish.com/2012/box-sizing-border-box-ftw/',
-  'https://github.com/nostalgiaz/bootstrap-toggle-buttons',
+  'https://github.com/nostalgiaz/bootstrap-toggle-buttons'
+]
+
+beib_urls = [
   'http://jetstrap.com/',
   'http://blog.8thlight.com/uncle-bob/2012/09/06/I-am-Your-New-CTO.html',
   'http://worrydream.com/LearnableProgramming/?utm_source=statuscode',
@@ -24,7 +34,10 @@ urls = [
   'http://diythemes.com/thesis/fun-fonts-logo/',
   'http://css-tricks.com/dont-overthink-it-grids/',
   'http://www.scribd.com/doc/232915/Ruby-PDFWriter-manual-pdf',
-  'http://www.tenbytwenty.com/sosa.php',
+  'http://www.tenbytwenty.com/sosa.php'
+]
+
+carly_urls = [
   'http://help.airbrake.io/kb/troubleshooting-2/javascript-notifier',
   'http://ricostacruz.com/jquery.transit/',
   'http://vim-adventures.com/',
@@ -33,10 +46,29 @@ urls = [
   'http://www.telestream.net/screen-flow/demos.htm'
 ]
 
-urls.each do |url|
+my_urls.each do |url|
   p url
-  Seeder.create_codemark(url, gmassanek)
+  VCR.use_cassette('seeds') do
+    Seeder.create_codemark(url, gmassanek)
+  end
 end
 
-Seeder.create_codemark('http://www.twitter.com/jbeiber', jbeiber)
-Seeder.create_codemark('http://www.twitter.com/carlyraejepsen', carlyraejepsen)
+beib_urls.each do |url|
+  p url
+  VCR.use_cassette('seeds') do
+    Seeder.create_codemark(url, jbeiber)
+  end
+end
+
+carly_urls.each do |url|
+  p url
+  VCR.use_cassette('seeds') do
+    Seeder.create_codemark(url, carlyraejepsen)
+  end
+end
+
+VCR.use_cassette('seeds_snapshots') do
+  LinkRecord.all.each do |link|
+    SiteSnapshot.save_snapshot_for(link)
+  end
+end
--- lib/jobs/snap_link_job.rb
+++ lib/jobs/snap_link_job.rb
@@ -1,21 +1,8 @@
 class SnapLinkJob < Struct.new(:link)
   def perform
     return if ENV['RAILS_ENV']=='test'
-    p "Taking Snapshot for: #{link.url}"
+    return link.snapshot_url
 
-    filepath = "/tmp/snapshot_#{link.id}.jpeg"
-    if SiteSnapshot.save_picture(link.url, filepath)
-      snapshot = File.new(filepath, 'r').read
-      snapshot_url = RemoteStorage.upload_link_snapshot(snapshot, link.id)
-      link.update_attributes(:snapshot_url => snapshot_url)
-      File.delete(filepath)
-    else
-      link.update_attributes(:snapshot_url => 'assets/error_loading.png')
-    end
-  rescue Exception => e
-    p 'Failed to take snapshot'
-    p e
-    puts e.backtrace.first(10).join("\n")
-    link.update_attributes(:snapshot_url => 'assets/error_loading.png')
+    SiteSnapshot.save_snapshot_for(link)
   end
 end
