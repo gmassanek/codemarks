50eaa169afa3c521208bfd2141d00d6257e72f8a
Geoff Massanek <gmassanek@gmail.com>
2013-01-06 09:41:36 -0600

    Removing unused pages controller actions

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,2 @@
 class PagesController < ApplicationController
-  def landing
-    redirect_to codemarks_path if logged_in?
-    @codemarks = FindCodemarks.new(:current_user => current_user).codemarks
-  end
-
-  def codemarklet_test
-    render :layout => nil
-  end
 end
