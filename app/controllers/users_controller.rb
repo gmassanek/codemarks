4268eb322d628ae4b2e05e2757100af4fe8800b2
Geoff Massanek <gmassanek@gmail.com>
2013-01-12 16:54:43 -0600

    Paginate users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,7 +24,9 @@ class UsersController < ApplicationController
     @users = User.includes(:authentications).
       joins("LEFT JOIN (#{CodemarkRecord.select('user_id, count(*)').group(:user_id).to_sql}) cm_count ON users.id = cm_count.user_id").
       joins("LEFT JOIN (#{Click.select('user_id, count(*)').group(:user_id).to_sql}) click_count ON users.id = click_count.user_id").
-      select('users.*, COALESCE(cm_count.count, 0) as cm_count, COALESCE(click_count.count, 0) as click_count')
+      select('users.*, COALESCE(cm_count.count, 0) as cm_count, COALESCE(click_count.count, 0) as click_count').
+      page(params[:page]).
+      per(50)
     @users.sort_by! { |u| u.cm_count.to_i + u.click_count.to_i }.reverse!
   end
 
