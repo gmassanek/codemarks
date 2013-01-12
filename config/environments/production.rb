2cc9556878ea05b92d263b8b1465d65ae93acb8e
Geoff Massanek <gmassanek@gmail.com>
2013-01-12 14:39:23 -0600

    Add production deploy settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Codemarks::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( backbone_app.js, user_show.js, user_edit.js )
+  config.assets.precompile += %w(google_analytics.js, uservoice.js)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
