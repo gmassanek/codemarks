4021eaa1e9da40b9ec6852f8454e9924fd0c4264
Geoff Massanek <gmassanek@gmail.com>
2013-01-14 18:12:13 -0600

    Debug github auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   #provider :provider, consumer_key, consumer_secret
+  p ENV['CODEMARK_GITHUB_KEY']
+  p ENV['CODEMARK_GITHUB_SECRET']
   provider :twitter, ENV['CODEMARK_TWITTER_KEY'], ENV['CODEMARK_TWITTER_SECRET']
   provider :github, ENV['CODEMARK_GITHUB_KEY'], ENV['CODEMARK_GITHUB_SECRET']
 end
