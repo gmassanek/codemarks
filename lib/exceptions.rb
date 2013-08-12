2203ca3fd688186932bd8fab2c5566d4db728499
Geoff Massanek <gmassanek@gmail.com>
2013-08-12 00:37:58 -0400

    Remove unused exceptions

--- lib/exceptions.rb
+++ lib/exceptions.rb
@@ -1,9 +1,5 @@
 module Exceptions
-  class LinkRequiredError < StandardError; end
-  class TopicsRequiredError < StandardError; end
   class UserRequiredError < StandardError; end
   class AuthHashRequiredError < StandardError; end
   class AuthProviderRequiredError < StandardError; end
-  class ValidURLRequiredError < StandardError; end
-  class InvalidLinkError < StandardError; end
 end
