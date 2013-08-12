62aac7e274c1cc0830ce6cb3904b33693c776179
Geoff Massanek <gmassanek@gmail.com>
2013-08-11 23:12:22 -0400

    Remove links controller since clicks are on resources now

--- app/controllers/links_controller.rb
+++
@@ -1,7 +0,0 @@
-class LinksController < ApplicationController
-  def click
-    @link = LinkRecord.find(params[:id])
-    Click.create(:link_record => @link, :user => current_user)
-    render :nothing => true, :status => :ok
-  end
-end
