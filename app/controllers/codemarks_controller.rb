b663795c736fa66724d02d4379744024c6ae8122
Geoff Massanek <gmassanek@gmail.com>
2013-08-12 18:17:21 -0400

    Head 200 for sendgrid

--- app/controllers/codemarks_controller.rb
+++ app/controllers/codemarks_controller.rb
@@ -31,6 +31,8 @@ class CodemarksController < ApplicationController
   def sendgrid
     p params
     Rails.logger.info params.inspect
+    head 200
+    render :json => { success: true }
   end
 
   def index
