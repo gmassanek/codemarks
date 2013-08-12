9a7025498c79777007bfa484883c85302d2de4b3
Geoff Massanek <gmassanek@gmail.com>
2013-08-11 23:33:41 -0400

    Remove taggable class

--- spec/fast_helper.rb
+++ spec/fast_helper.rb
@@ -1,6 +1,5 @@
 require 'active_support/core_ext' #to get .blank?
 
 require_relative '../app/lib/tagger'
-require_relative '../app/lib/taggable'
 require_relative '../app/lib/find_codemarks'
 require_relative '../app/lib/resource'
