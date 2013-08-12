e2438eeaf32b82aab0adb6cc4a6ba15b3b6caf27
Geoff Massanek <gmassanek@gmail.com>
2013-08-12 00:41:43 -0400

    Remove unused initializers

--- config/initializers/logger_fix.rb
+++
@@ -1,1 +0,0 @@
-Rails.logger.instance_variable_get(:@logger).instance_variable_get(:@log_dest).sync = true if Rails.logger
--- config/initializers/template_asset_preprocessor.rb
+++
@@ -1,6 +0,0 @@
-class TemplateAssetPreprocessor < Sprockets::Processor
-  def evaluate(context, locals)
-    p context
-    RAILS.logger.info context.inspect
-  end
-end
