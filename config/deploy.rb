a2f7893bac1508e8ffe6246defa0e7053a9ae749
Geoff Massanek <gmassanek@gmail.com>
2013-11-02 22:18:31 -0500

    Update deploy warning

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,7 +48,7 @@ namespace:deploy do
   desc "Make sure local git is in sync with remote."
   task :check_revision, roles: :web do
     unless `git rev-parse HEAD` == `git rev-parse origin/#{branch}`
-      puts "WARNING: HEAD is not the same as origin/#{branch}"
+      puts "HALTED DEPLOY: HEAD is not the same as origin/#{branch}"
       puts "Run `git push` to sync changes."
       exit
     end
