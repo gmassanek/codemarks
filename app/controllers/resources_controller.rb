4dc083e3ca2fc2cd046294e3cf50db986699f5a7
Geoff Massanek <gmassanek@gmail.com>
2014-01-05 22:14:46 -0600

    Record user click

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -2,6 +2,10 @@ class ResourcesController < ApplicationController
   def click
     resource = params[:resource_type].classify.constantize.find(params[:id])
     Click.create(:resource => resource, :user => current_user)
+    data = {
+      :resource_id => resource.id
+    }
+    Global.track(:user_id => current_user.nickname, :event => 'codemark_visit', :properties => data)
     render :nothing => true, :status => :ok
   end
 end
